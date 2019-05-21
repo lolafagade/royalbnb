e0db7579fd581e2c32c4b70df3c788e41c1f1ad9
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-21 15:42:51 +0200

    changed jasmine palace pic

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ jasmine_castle = Castle.create!(name: "The Sultan's Palace", description: "Agrab
   of enchantment with its sorcerers, genies, and flying carpets.
 Agrabah is based on and is largely inspired by the Iraqi city of Baghdad during the Islamic Golden Age.",
 prince: 'Alladin', location: 'Agrabah', equipment: 'genie in a lamp, tiger', vehicle: 'flying carpet', price_per_night: '5',
-user_id: jasmine.id, photo: 'https://res.cloudinary.com/dl0kdumil/image/upload/v1558438134/jc3kgtdtrgyj816itmxz.jpg')
+user_id: jasmine.id, photo: 'xaorewukh3xrmznlycff.jpg')
 
 elsa_castle = Castle.create!(name: "The Fair Kingdom", description: "In Arendelle's fair kingdom you can enjoy the beautiful mountains for sun tanning and to go skiing",
 prince: 'Hot prince', location: 'Nordicway', equipment: 'Snow men, birds that can help you get dressed', vehicle: 'A sled ', price_per_night: '6',
